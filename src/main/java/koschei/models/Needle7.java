1011a39c39a1dd6a464aef1afdd4240bc265874a
Terpsihoransk <arsenjeva.ea@gmail.com>
2022-05-01 23:16:11 +0700

    Initial commit

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,18 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+
+    private Deth8 deth;
+
+    @Override
+    public String toString() {
+        return "в яйце — игла, " + deth.toString();
+    }
+
+    @Autowired
+    public void setDeth(Deth8 deth) { this.deth = deth; }
+}
